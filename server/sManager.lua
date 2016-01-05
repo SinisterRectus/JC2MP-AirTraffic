459828897b6511b70edb0d00a1617477ea9696b6
SinisterRectus <SinisterRectus@users.noreply.github.com>
2016-01-05 17:11:45 -0500

    Incorporated new settings

--- server/sManager.lua
+++ server/sManager.lua
@@ -2,27 +2,10 @@ class 'AirTrafficManager'
 
 function AirTrafficManager:__init()
 
-	self.delay = 2 -- seconds
 	self.timer = Timer()
 	
 	self.npcs = {}
 	self.count = 0
-	
-	self.models = {
-		civilian = {39, 51, 59, 81},
-		military = {30, 34, 85},
-	}
-	
-	self.speeds = {
-		[24] = 82, -- F-33 DragonFly
-		[30] = 77, -- Si-47 Leopard
-		[34] = 95, -- G9 Eclipse
-		[39] = 90, -- Aeroliner 474
-		[51] = 69, -- Cassius 192
-		[59] = 56, -- Peek Airhawk 225
-		[81] = 73, -- Pell Silverbolt 6
-		[85] = 87, -- Bering I-86DP
-	}
 	
 	Events:Subscribe("ModuleLoad", self, self.ModuleLoad)
 	Events:Subscribe("ModuleUnload", self, self.ModuleUnload)
@@ -37,7 +20,7 @@ function AirTrafficManager:ModuleLoad()
 
 	local timer = Timer()
 	
-	for i = 1, 1024 do
+	for i = 1, settings.count do
 		self:SpawnRandomNPC()
 	end
 	
@@ -58,7 +41,10 @@ end
 
 function AirTrafficManager:PostTick(args)
 
-	local n = math.min(args.delta * self.count / self.delay, self.count)
+	local count = self.count
+	local delay = settings.delay
+
+	local n = math.min(args.delta * count / delay, count)
 	
 	if n > 1 then
 
@@ -66,7 +52,7 @@ function AirTrafficManager:PostTick(args)
 			coroutine.resume(self.co)
 		end
 		
-	elseif self.timer:GetSeconds() > self.delay / self.count then
+	elseif self.timer:GetSeconds() > delay / count then
 
 		self.timer:Restart()
 		coroutine.resume(self.co)
@@ -80,7 +66,7 @@ function AirTrafficManager:Collision(args)
 	local npc = self.npcs[args.id]
 	if not npc then return end
 
-	npc.vehicle:SetHealth(0)
+	npc:Remove()
 
 end
 
@@ -124,13 +110,13 @@ end
 function AirTrafficManager:SpawnRandomNPC()
 
 	local angle = Angle(math.pi * 0.1 * math.random(-10, 10), 0, 0)
-	local model_id = table.randomvalue(self.models.civilian)
-
+	local model_id = table.randomvalue(settings.pool)
+	
 	AirTrafficNPC({
 		model_id = model_id,
 		position = Vector3(math.random(-16384, 16384), math.random(0, 100), math.random(-16384, 16384)),
 		angle = angle,
-		linear_velocity = angle * Vector3.Forward * self.speeds[model_id]
+		linear_velocity = angle * Vector3.Forward * settings.speeds[model_id]
 	})
 
 end
